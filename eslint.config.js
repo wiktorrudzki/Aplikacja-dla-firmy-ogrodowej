4c26dacb00c3117739d4801f661f4caec8330295
Jakub Niewelt <jakubniewelt@icloud.com>
2025-03-22 11:41:07 +0100

    chore: configure prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+    },
+  },
+];
