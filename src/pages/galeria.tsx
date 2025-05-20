0ded22d8211f6ede0d0babffd9288fbef7e1853c
Jakub Niewelt <jakubniewelt@icloud.com>
2025-05-20 23:38:40 +0200

    feat: sample image in gallery

--- src/pages/galeria.tsx
+++ src/pages/galeria.tsx
@@ -1,5 +1,47 @@
-const Gallery = () => {
-  return <div>Gallery</div>;
+import React from "react";
+import { graphql, PageProps } from "gatsby";
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
+import { galleryImagesData } from "@data/gallery-images";
+
+type ImageNode = {
+  relativePath: string;
+  childImageSharp: {
+    gatsbyImageData: ImageDataLike;
+  };
+};
+
+interface DataType {
+  images: {
+    nodes: ImageNode[];
+  };
+}
+
+const Gallery = ({ data }: PageProps<DataType>) => {
+  const imageData = galleryImagesData[0];
+  const node = data.images.nodes.find(
+    (image) => image.relativePath === imageData.src,
+  );
+  const image = getImage(node?.childImageSharp?.gatsbyImageData!);
+
+  return (
+    <div>
+      <h1>Gallery</h1>
+      <GatsbyImage image={image!} alt={imageData.alt} />
+    </div>
+  );
 };
 
 export default Gallery;
+
+export const pageQuery = graphql`
+  {
+    images: allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+      nodes {
+        relativePath
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+        }
+      }
+    }
+  }
+`;
