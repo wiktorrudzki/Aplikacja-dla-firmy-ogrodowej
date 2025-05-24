4acd730ca78f20f6a590793fa11d1618b57b8c86
Jakub Niewelt <jakubniewelt@icloud.com>
2025-05-25 00:20:00 +0200

    feat: add regulamin page

---
+++ src/pages/regulamin.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Regulations = () => {
+  return <Box height="125vh">Regulations</Box>;
+};
+
+export default Regulations;
