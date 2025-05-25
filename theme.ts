2aed9d9349e7bacf8203e671f6471ed97d5995b1
Jakub Niewelt <jakubniewelt@icloud.com>
2025-05-25 11:15:19 +0200

    fix(ChakraUI): extend default config

--- theme.ts
+++ theme.ts
@@ -1,8 +1,4 @@
-import {
-  createSystem,
-  defaultBaseConfig,
-  defineConfig,
-} from "@chakra-ui/react";
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 
 const customConfig = defineConfig({
   theme: {
@@ -56,14 +52,6 @@ const customConfig = defineConfig({
         },
       },
     },
-    breakpoints: {
-      base: "0em", // 0px
-      sm: "30em", // ~480px
-      md: "48em", // ~768px
-      lg: "62em", // ~992px
-      xl: "80em", // ~1280px
-      "2xl": "96em", // ~1536px
-    },
     tokens: {
       colors: {
         green: {
@@ -83,4 +71,4 @@ const customConfig = defineConfig({
   },
 });
 
-export const system = createSystem(defaultBaseConfig, customConfig);
+export const system = createSystem(defaultConfig, customConfig);
