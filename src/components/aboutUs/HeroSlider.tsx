c08753217bb0ece542927e5ff61a786de450725d
Wiktor <wiktor1rudzki@gmail.com>
2025-06-11 17:15:56 +0200

    refactor: useStaticQuery in custom hook

--- src/components/aboutUs/HeroSlider.tsx
+++ src/components/aboutUs/HeroSlider.tsx
@@ -6,36 +6,36 @@ import "swiper/css";
 import "./HeroSlider.css";
 import SliderControls from "./SliderControls";
 import Slide from "./Slide";
-import { Slide as SlideType } from "@src/types/slide";
+import { useOurValues } from "@src/hooks";
 
-type Props = {
-  slides: SlideType[];
+const HeroSlider = () => {
+  const slides = useOurValues();
+
+  return (
+    <Box overflowX="hidden">
+      <Swiper
+        modules={[Autoplay, Navigation, Pagination]}
+        pagination={{
+          el: ".swiper-pagination",
+          type: "progressbar",
+          progressbarFillClass: "about-us-progress-fill",
+        }}
+        navigation={{
+          nextEl: ".about-us-swiper-button-next",
+          prevEl: ".about-us-swiper-button-prev",
+        }}
+        autoplay={{ delay: 8000 }}
+        style={{ height: "100svh" }}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <Slide slide={slide} />
+          </SwiperSlide>
+        ))}
+        <SliderControls />
+      </Swiper>
+    </Box>
+  );
 };
-
-const HeroSlider = ({ slides }: Props) => (
-  <Box overflowX="hidden">
-    <Swiper
-      modules={[Autoplay, Navigation, Pagination]}
-      pagination={{
-        el: ".swiper-pagination",
-        type: "progressbar",
-        progressbarFillClass: "about-us-progress-fill",
-      }}
-      navigation={{
-        nextEl: ".about-us-swiper-button-next",
-        prevEl: ".about-us-swiper-button-prev",
-      }}
-      autoplay={{ delay: 8000 }}
-      style={{ height: "100svh" }}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <Slide slide={slide} />
-        </SwiperSlide>
-      ))}
-      <SliderControls />
-    </Swiper>
-  </Box>
-);
 
 export default HeroSlider;
