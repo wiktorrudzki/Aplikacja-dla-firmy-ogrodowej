237b469836efd31c593b770455a4c6821ba25bcc
Wiktor <wiktor1rudzki@gmail.com>
2025-06-11 17:15:56 +0200

    refactor: useStaticQuery in custom hook

--- src/pages/o-nas.tsx
+++ src/pages/o-nas.tsx
@@ -1,30 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { HeroSlider } from "@src/components/aboutUs";
-import { GraphQL } from "@src/types/grapql";
 import { GatsbyPageWithLayout } from "@src/types/page";
-import { Slide } from "@src/types/slide";
-import { graphql, PageProps } from "gatsby";
+
 import React from "react";
 
-const About: GatsbyPageWithLayout<PageProps<GraphQL<Slide>>> = ({
-  data: { data: slides },
-}) => (
+const About: GatsbyPageWithLayout = () => (
   <Box>
-    <HeroSlider slides={slides.nodes.map((s) => s.frontmatter)} />
+    <HeroSlider />
   </Box>
 );
 
 export default About;
-
-export const pageQuery = graphql`
-  {
-    data: allMdx(
-      filter: { internal: { contentFilePath: { regex: "/our-values/" } } }
-      sort: { frontmatter: { id: ASC } }
-    ) {
-      nodes {
-        ...OurValues
-      }
-    }
-  }
-`;
