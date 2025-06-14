23421053d672c1e0aacc00efc34c5ef610b37dfc
Jakub Niewelt <jakubniewelt@icloud.com>
2025-06-14 18:19:36 +0200

    feat: connect ImageJson to ImageSharp

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,7 @@
-import { GatsbyNode } from "gatsby";
+import { GatsbyNode, Node } from "gatsby";
 import MillionLint from "@million/lint";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import { ImageJsonNode } from "@src/types/graphql";
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   stage,
@@ -14,4 +15,53 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   actions.setWebpackConfig({
     plugins,
   });
+};
+
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
+  ({ actions }) => {
+    const { createTypes } = actions;
+    const typeDefs = `
+    type ImageJson implements Node {
+      title: String!
+      altKey: String!
+      relativePath: String!
+      childImageSharp: ImageSharp
+    }
+  `;
+    createTypes(typeDefs);
+  };
+
+const createNodeMap = <T extends Node>(
+  nodes: T[],
+  key: keyof T,
+): Map<string, T> => {
+  const map = new Map<string, T>();
+  nodes.forEach((node) => {
+    const keyValue = node[key] as string;
+    if (keyValue) map.set(keyValue, node);
+  });
+  return map;
+};
+
+export const createResolvers: GatsbyNode["createResolvers"] = ({
+  createResolvers,
+  getNodesByType,
+  getNode,
+}) => {
+  const images = getNodesByType("File").filter(
+    (node) => node.sourceInstanceName === "images",
+  );
+  const imageMap = createNodeMap(images, "relativePath");
+  const ImageJson = {
+    childImageSharp: {
+      type: "ImageSharp",
+      resolve: (source: ImageJsonNode) =>
+        imageMap
+          .get(source.relativePath!)
+          ?.children.map(getNode)
+          .find((child) => child?.internal?.type === "ImageSharp"),
+    },
+  };
+
+  createResolvers({ ImageJson });
 };
--- src/types/graphql.ts
+++ src/types/graphql.ts
@@ -1,5 +1,15 @@
+import { Node } from "gatsby";
+import { ImageDataLike } from "gatsby-plugin-image";
+
 export type GraphQLMdxNodes<T extends {}> = {
   data: {
     nodes: { frontmatter: T }[];
   };
+};
+
+export type ImageJsonNode = Node & {
+  title?: string;
+  altKey?: string;
+  relativePath?: string;
+  childImageSharp?: { gatsbyImageData: ImageDataLike };
 };
