0a1d05459f24a194e484f3c7e8b33076cdb1ca4c
Wiktor <wiktor1rudzki@gmail.com>
2025-06-26 18:19:03 +0200

    fix: styles - sizes, paddings

--- src/components/aboutUs/OurServiceContent.tsx
+++ src/components/aboutUs/OurServiceContent.tsx
@@ -2,7 +2,7 @@ import { Box, VStack } from "@chakra-ui/react";
 import React from "react";
 import { MainCard } from "../main-card";
 import { Heading1, Paragraph } from "../typography";
-import { ContentCardButton } from "../content-card";
+import { ContentCard, ContentCardButton } from "../content-card";
 import { t } from "@src/utils/i18n";
 import { OurService } from "@src/types/services";
 
@@ -11,17 +11,13 @@ type Props = {
 };
 
 const OurServiceContent = ({ service }: Props) => (
-  <Box flex="1">
-    <MainCard h="100%">
-      <VStack h="100%" justify="center" gap={{ base: 4, lg: 8, xl: 16 }}>
-        <Heading1 textAlign="center">{service.title}</Heading1>
-        <Paragraph textAlign="center">{service.aboutDescription}</Paragraph>
-        <ContentCardButton to={service.route}>
-          {t("Poznaj ofertę")}
-        </ContentCardButton>
-      </VStack>
-    </MainCard>
-  </Box>
+  <ContentCard m={0} gap={[6, 12]}>
+    <Heading1 textAlign="center">{service.title}</Heading1>
+    <Paragraph textAlign="center">{service.aboutDescription}</Paragraph>
+    <ContentCardButton to={service.route}>
+      {t("Poznaj ofertę")}
+    </ContentCardButton>
+  </ContentCard>
 );
 
 export default OurServiceContent;
--- src/components/aboutUs/OurServiceImage.tsx
+++ src/components/aboutUs/OurServiceImage.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren } from "react";
 type Props = PropsWithChildren;
 
 const OurServiceImage = ({ children }: Props) => (
-  <Box flex="1" rounded={32} overflow="hidden" boxShadow="element">
+  <Box rounded={32} my={5} mx={[5]} overflow="hidden" boxShadow="element">
     {children}
   </Box>
 );
