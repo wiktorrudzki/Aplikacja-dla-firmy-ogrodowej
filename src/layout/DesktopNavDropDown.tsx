12fcaad7476b7c3a54accff918932bfe7e95329c
Jakub Niewelt <jakubniewelt@icloud.com>
2025-06-26 22:06:44 +0200

    feat: add desktop dropdown

---
+++ src/layout/DesktopNavDropDown.tsx
@@ -0,0 +1,61 @@
+import { Box, HStack, useToken, VStack } from "@chakra-ui/react";
+import { NAVIGATION_MODE } from "@src/types/navigation";
+import React from "react";
+import { BiChevronDown } from "react-icons/bi";
+import CustomLink from "./Link";
+
+type Props = {
+  label: string;
+  mode: NAVIGATION_MODE;
+  children: React.ReactNode;
+};
+
+const DesktopNavDropDown = ({ label, mode, children }: Props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [transparentBlack] = useToken("colors", "blackAlpha.600");
+  const timeoutRef = React.useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const open = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    setIsOpen(true);
+  };
+
+  const close = () => setIsOpen(false);
+
+  const deferClose = () => {
+    timeoutRef.current = setTimeout(close, 200);
+  };
+
+  return (
+    <Box position="relative">
+      <CustomLink mode={mode} to="#">
+        <HStack onMouseEnter={open} onMouseLeave={deferClose}>
+          {label}
+          <BiChevronDown />
+        </HStack>
+      </CustomLink>
+      {isOpen && (
+        <Box
+          position="absolute"
+          left="calc(50%)"
+          marginTop="4"
+          padding="6"
+          bgColor={mode === NAVIGATION_MODE.DARK ? "white" : transparentBlack}
+          width="max-content"
+          transform="translateX(-50%)"
+          textAlign="center"
+          shadow="element"
+          borderRadius="sm"
+          onMouseEnter={open}
+          onMouseLeave={deferClose}
+        >
+          <VStack gap="6" onClick={close}>
+            {children}
+          </VStack>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default DesktopNavDropDown;
