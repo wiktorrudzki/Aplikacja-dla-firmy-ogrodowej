2251e9bd656bba05511489815bf6135a65de5531
Wiktor <wiktor1rudzki@gmail.com>
2025-06-26 18:39:49 +0200

    add: background gradient instead of filter brightness

--- src/components/aboutUs/WorkEffectSlide.tsx
+++ src/components/aboutUs/WorkEffectSlide.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import { ImageJsonNode } from "@src/types/graphql";
 import { t } from "@src/utils/i18n";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
@@ -9,16 +10,24 @@ type Props = {
 
 const WorkEffectSlide = ({ image }: Props) =>
   getImage(image.childImageSharp ?? null) != undefined ? (
-    <GatsbyImage
-      image={getImage(image.childImageSharp ?? null)!}
-      alt={t(image.altKey)}
-      style={{
-        position: "fixed",
-        height: "100%",
-        width: "100%",
-        filter: "brightness(0.75)",
-      }}
-    />
+    <>
+      <GatsbyImage
+        image={getImage(image.childImageSharp ?? null)!}
+        alt={t(image.altKey)}
+        style={{
+          position: "fixed",
+          height: "100%",
+          width: "100%",
+        }}
+      />
+      <Box
+        w="100%"
+        h="100%"
+        pos="absolute"
+        opacity={0.5}
+        background="linear-gradient(to bottom, black, transparent)"
+      />
+    </>
   ) : null;
 
 export default WorkEffectSlide;
