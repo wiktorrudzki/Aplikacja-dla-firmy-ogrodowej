2d83a0ed38b64f189d8258877d9188df5b92fcb6
JN0122 <jakubniewelt@icloud.com>
2025-06-27 16:05:20 +0200

    feat: move content card values to useResponsiveValues hook

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,1 @@
-export default {
-  navigationBaseSizeRem: 1.5 * 2 + 3,
-  navigationMdSizeRem: 1.5 * 2 + 6,
-  contactCardIconBaseOffsetRem: 2,
-  contactCardIconMdOffsetRem: 2.5,
-  contactCardIconBaseSizeRem: 3,
-  contactCardIconMdSizeRem: 4,
-};
-
 export { default as ROUTES } from "./routes";
