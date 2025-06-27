4489416fe85d7d8422cde851fd718a1aa08b1caf
JN0122 <jakubniewelt@icloud.com>
2025-06-27 16:04:53 +0200

    feat: create useResponsiveValues hook, handle nav height

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useNavigationMode } from "./useNavigationMode";
 export { default as useOurValues } from "./useOurValues";
 export { default as useSiteMetadata } from "./useSiteMetadata";
 export { default as useWorkEffects } from "./useWorkEffects";
+export { default as useResponsiveValues } from "./useResponsiveValues";
