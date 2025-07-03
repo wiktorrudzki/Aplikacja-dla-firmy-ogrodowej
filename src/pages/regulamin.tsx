1cbf5c8f887fce1b7f808a54971fce19f0a6413c
Jakub Niewelt <jakubniewelt@icloud.com>
2025-07-03 13:34:30 +0200

    feat: style regulamin page

--- src/pages/regulamin.tsx
+++ src/pages/regulamin.tsx
@@ -1,11 +1,16 @@
-import { Box } from "@chakra-ui/react";
 import { SEO } from "@src/components/seo";
 import { t } from "@i18n";
 import { HeadFC } from "gatsby";
 import React from "react";
+import { MainContainer } from "@src/components/main-container";
+import { Heading1 } from "@src/components/typography";
 
 const Regulations = () => {
-  return <Box height="125vh">Regulations</Box>;
+  return (
+    <MainContainer>
+      <Heading1>Regulamin</Heading1>
+    </MainContainer>
+  );
 };
 
 export default Regulations;
