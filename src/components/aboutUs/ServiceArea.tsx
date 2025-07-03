6cb8e571cf691a62ce14ccb8ee7da52d176eb79e
Jakub Niewelt <jakubniewelt@icloud.com>
2025-07-03 13:14:09 +0200

    feat: standarize about us margins, display components as section

--- src/components/aboutUs/ServiceArea.tsx
+++ src/components/aboutUs/ServiceArea.tsx
@@ -4,25 +4,28 @@ import { Separator, Stack } from "@chakra-ui/react";
 import { Heading1, Paragraph } from "../typography";
 import { t } from "@src/utils/i18n";
 import { GoogleMap } from "../google-map";
+import { ContainerSection } from "../section";
 
 const ServiceArea = () => {
   return (
-    <MainCard my={["8", "12", "16"]} maxW={{ base: "90%", xl: "7xl" }}>
-      <Stack gap={8}>
-        <Separator
-          margin="0 auto"
-          w="20%"
-          size="lg"
-          borderColor="green.500"
-          rounded={32}
-        />
-        <Heading1 textAlign="center">{t("service-area-title")}</Heading1>
-        <Paragraph textAlign="center">
-          {t("service-area-description")}
-        </Paragraph>
-        <GoogleMap />
-      </Stack>
-    </MainCard>
+    <ContainerSection>
+      <MainCard maxW={{ base: "90%", xl: "7xl" }}>
+        <Stack gap={8}>
+          <Separator
+            margin="0 auto"
+            w="20%"
+            size="lg"
+            borderColor="green.500"
+            rounded={32}
+          />
+          <Heading1 textAlign="center">{t("service-area-title")}</Heading1>
+          <Paragraph textAlign="center">
+            {t("service-area-description")}
+          </Paragraph>
+          <GoogleMap />
+        </Stack>
+      </MainCard>
+    </ContainerSection>
   );
 };
 
