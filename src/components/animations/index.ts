767f326cb3b6ffd123701c7ba14f00787af1ed36
Jakub Niewelt <jakubniewelt@icloud.com>
2025-07-15 23:24:51 +0200

    feat: animate cursor spotlight on hover

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -1,3 +1,4 @@
 export { default as AnimateCounter } from "./AnimateCounter";
 export { default as AnimateBlock } from "./AnimateBlock";
 export { default as AnimateSlideToViewFixed } from "./AnimateSlideToViewFixed";
+export { default as AnimateCursorSpotlightOnHover } from "./AnimateCursorSpotlightOnHover";
