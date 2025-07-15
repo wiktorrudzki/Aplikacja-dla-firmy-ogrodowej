5bbaeda97f141c3a7734e8b03387e3176e6c18fd
Wiktor <wiktor1rudzki@gmail.com>
2025-07-15 19:13:34 +0200

    add: grip icon in the estimate card

--- src/components/estimate-card/EstimateCard.tsx
+++ src/components/estimate-card/EstimateCard.tsx
@@ -11,37 +11,60 @@ import { motion } from "framer-motion";
 import { MdPhone } from "react-icons/md";
 import { Heading2, Paragraph } from "../typography";
 import { t } from "@src/utils/i18n";
+import { LuGripVertical } from "react-icons/lu";
 
 const MotionBox = motion(Box);
 
 const EstimateCard = () => {
   const [green] = useToken("colors", ["green.500"]);
 
-  const panelWidth = useBreakpointValue({ base: 225, lg: 250 });
-  const closedOffset = useBreakpointValue({ base: 215, lg: 255 });
+  const panelWidth = useBreakpointValue({ base: 250, lg: 300 });
+  const closedOffset = useBreakpointValue({ base: 236, lg: 288 });
 
   return (
     <MotionBox
       position="fixed"
-      bottom={{ base: 2, md: 4, "2xl": 8 }}
-      right={{ base: 2, md: 4, "2xl": 8 }}
-      zIndex={1}
+      bottom={{ base: 2, md: 4 }}
+      right={{ base: 2, md: 4 }}
+      zIndex={10}
       drag="x"
       dragConstraints={{ left: 0, right: closedOffset }}
       animate={{ x: 0 }}
       width={panelWidth}
       cursor="grab"
+      display="flex"
+      alignItems="center"
     >
+      <Box
+        bg="green.500"
+        height="64px"
+        width="16px"
+        borderTopLeftRadius="xl"
+        borderBottomLeftRadius="xl"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        zIndex={20}
+      >
+        <LuGripVertical color="white" size={20} />
+      </Box>
+
       <Stack
-        position="relative"
         bg="green.50"
         rounded="2xl"
         py={4}
-        px={{ base: 4, lg: 6 }}
+        px={{ base: 6, lg: 10 }}
         boxShadow="based"
         alignItems="center"
+        width="100%"
+        gap={{ base: 1, md: 2 }}
       >
-        <Paragraph textAlign="center">{t("Skorzystaj z darmowej")}</Paragraph>
+        <Paragraph
+          lineHeight={{ base: "shorter", md: "short" }}
+          textAlign="center"
+        >
+          {t("Skorzystaj z darmowej")}
+        </Paragraph>
         <Paragraph>{t("wyceny")}</Paragraph>
         <Link
           href={`tel:${t("telephone-number")}`}
