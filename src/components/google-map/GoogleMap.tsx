3c3545344e863833f039445668a6ef88d94c41a1
Wiktor <wiktor1rudzki@gmail.com>
2025-09-16 18:54:40 +0200

    add: google map iframe

--- src/components/google-map/GoogleMap.tsx
+++ src/components/google-map/GoogleMap.tsx
@@ -2,8 +2,13 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 
 const GoogleMap = () => (
-  <Box textAlign="center" bg="white" width="100%" height="300px">
-    TODO - replace with a google map
+  <Box textAlign="center" bg="white" width="100%" height="450px">
+    <iframe
+      src="https://www.google.com/maps/d/u/4/embed?mid=12oViRAr61nRjy0h2XUktryjkTMqw4gA&ehbc=2E312F&noprof=1"
+      width="100%"
+      height="100%"
+      title="Zakres usług"
+    ></iframe>
   </Box>
 );
 
