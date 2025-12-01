16735b438e13a6db01c28784511fbe62248f2d57
Wiktor <wiktor1rudzki@gmail.com>
2025-12-01 23:33:56 +0100

    add: notification after submitting contact form

--- app/src/components/contact/ContactForm.tsx
+++ app/src/components/contact/ContactForm.tsx
@@ -6,30 +6,17 @@ import { Box } from "@chakra-ui/react";
 import { ContactFormInputs, ContactFromResponse } from "@src/types/form";
 import { RadialBackgroundContainer } from "../radial-background-container";
 import { useWithLoader } from "@src/hooks";
-<<<<<<< HEAD
 import { Alert } from "../alert";
-=======
-import { toaster } from "../ui/toaster";
->>>>>>> a2715b2 (add: contact form validation and onFinish)
 
 const CONTACT_FORM_API = process.env.GATSBY_CONTACT_FORM_API;
 
 const ContactForm = () => {
-<<<<<<< HEAD
   const [isSent, setIsSent] = useState(false);
 
   const [onFinish, isLoading] = useWithLoader(
     async (values: ContactFormInputs) => {
-      console.log("dasdassad");
-
       if (!CONTACT_FORM_API) return;
 
-=======
-  const [onFinish, isLoading] = useWithLoader(
-    async (values: ContactFormInputs) => {
-      if (!CONTACT_FORM_API) return;
-
->>>>>>> a2715b2 (add: contact form validation and onFinish)
       await fetch(CONTACT_FORM_API, {
         method: "POST",
         headers: {
@@ -39,18 +26,7 @@ const ContactForm = () => {
         body: JSON.stringify(values),
       })
         .then((res: Response) => res.json())
-<<<<<<< HEAD
         .then((data: ContactFromResponse) => data.success && setIsSent(true))
-=======
-        .then(
-          (data: ContactFromResponse) =>
-            data.success &&
-            toaster.create({
-              title: t("Sukces!"),
-              description: t("Twoja wiadomość została wysłana."),
-            }),
-        )
->>>>>>> a2715b2 (add: contact form validation and onFinish)
         .catch((err) => console.error(err));
     },
   );
