305f8fb8dd528a859448d5497b840a019c0a10a5
Jakub Niewelt <jakubniewelt@icloud.com>
2025-12-02 00:07:05 +0100

    feat: move default gallery into nested path

--- app/src/constants/routes.ts
+++ app/src/constants/routes.ts
@@ -2,7 +2,7 @@ enum ROUTES {
   HOME = "/",
   O_NAS = "/o-nas",
   KONTAKT = "/kontakt",
-  GALERIA = "/galeria",
+  GALERIA = "/galeria/klient-indywidualny",
   DLA_FIRM = "/dla-firm",
   KLIENT_INDYWIDUALNY = "/klient-indywidualny",
   POLITYKA_PRYWATNOSCI = "/polityka-prywatnosci",
