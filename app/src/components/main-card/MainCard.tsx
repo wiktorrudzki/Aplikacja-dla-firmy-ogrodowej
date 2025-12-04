4ffa6b90292c9d31e4ba45f09c8171ec1f952be1
Jakub Niewelt <jakubniewelt@icloud.com>
2025-12-04 17:44:09 +0100

    feat: new siteimage, remove gradient from main card

--- app/src/components/main-card/MainCard.tsx
+++ app/src/components/main-card/MainCard.tsx
@@ -5,28 +5,22 @@ type Props = ContainerProps & {
   children: React.ReactNode;
 };
 
-const MainCard = ({ children, ...passProps }: Props) => {
-  const mainCard = useToken("colors", ["primary.50", "secondary.50"]);
-
-  return (
-    <Container
-      bgGradient={`radial-gradient(circle at bottom right, ${mainCard[0]}, ${mainCard[1]})`}
-      py={{
-        base: 8,
-        md: 8,
-        lg: 12,
-      }}
-      px={{
-        base: 8,
-        lg: 16,
-      }}
-      borderRadius={16}
-      boxShadow="element"
-      {...passProps}
-    >
-      {children}
-    </Container>
-  );
-};
+const MainCard = ({ children, ...passProps }: Props) => (
+  <Container
+    py={{
+      base: 8,
+      md: 8,
+      lg: 12,
+    }}
+    px={{
+      base: 8,
+      lg: 16,
+    }}
+    borderRadius={16}
+    {...passProps}
+  >
+    {children}
+  </Container>
+);
 
 export default MainCard;
